9c5e22ec250ed0e6cebb28967a11938867ff905f
Sergei <mindalev@ya.ru>
2024-08-23 23:51:37 +0300

    docker

--- users/admin.py
+++ users/admin.py
@@ -8,7 +8,6 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "email",)
 
 
-
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ('user', 'date_payment', 'lesson_paid', 'course_paid', 'amount')
